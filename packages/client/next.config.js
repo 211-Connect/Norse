20892e0ee78cb1be2a028afb94f05918e6a38b86
Chris Short <13677134+devcshort@users.noreply.github.com>
2023-06-19 23:55:37 -0700

    Clean up next config

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -2,10 +2,8 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
-const fs = require('fs-extra');
 const { i18n } = require('./next-i18next.config');
-const getAppTheme = require('./webpack/getAppTheme');
-const validateEnvironmentVariables = require('./webpack/validateEnvironmentVariables');
+const webpackConfig = require('./webpack');
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
@@ -20,44 +18,7 @@ const nextConfig = {
     locales: i18n.locales,
     defaultLocale: i18n.defaultLocale,
   },
-  webpack: (config, { isServer, dir, config: nextConfig }) => {
-    if (!isServer) {
-      validateEnvironmentVariables();
-
-      // Build the application theme file
-      // Seperate larger parts of the config in to individual files
-      const appConfigExists = fs.existsSync(`${dir}/app.json`);
-      const fileName = appConfigExists ? 'app.json' : 'app.defaults.json';
-
-      // Read the app config file and create a theme file
-      const appConfig = fs.readJSONSync(`${dir}/${fileName}`);
-      const appTheme = appConfig.theme;
-      const appThemeFile = `${dir}/.norse/theme.json`;
-      const theme = getAppTheme(appTheme);
-      try {
-        fs.writeJSONSync(appThemeFile, theme);
-        fs.writeJSONSync(`${dir}/.norse/categories.json`, appConfig.categories);
-        fs.writeJSONSync(
-          `${dir}/.norse/suggestions.json`,
-          appConfig.suggestions
-        );
-      } catch (err) {
-        process.exit(1);
-      }
-
-      if (
-        'nextConfig' in appConfig &&
-        Object.keys(appConfig.nextConfig).length > 0
-      ) {
-        nextConfig = {
-          ...nextConfig,
-          ...appConfig.nextConfig,
-        };
-      }
-    }
-
-    return config;
-  },
+  webpack: webpackConfig,
 };
 
 const plugins = [
