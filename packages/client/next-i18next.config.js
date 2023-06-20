0d3b192781487123a17f335872e09e41f36e88bd
Chris Short <13677134+devcshort@users.noreply.github.com>
2023-06-19 23:56:50 -0700

    Use actual config file that is generated during build time

--- packages/client/next-i18next.config.js
+++ packages/client/next-i18next.config.js
@@ -1,10 +1,11 @@
 // This file is REQUIRED by react-i18next/next-i18next. DO NOT DELETE
 const path = require('path');
+const config = require('./.norse/config.json');
 
 module.exports = {
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: config.i18n.defaultLocale,
+    locales: config.i18n.locales,
     localePath: path.resolve('./public/locales'),
   },
 };
