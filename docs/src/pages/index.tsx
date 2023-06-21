651c0fb7ba7d4c25c5fddd1d79f7b6655f4c4888
Chris Short <13677134+devcshort@users.noreply.github.com>
2023-06-20 18:33:46 -0700

    Updating description and title

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -45,8 +45,8 @@ export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title={siteConfig.title}
+      description="The modern search engine for community resources."
     >
       <HomepageHeader />
     </Layout>
