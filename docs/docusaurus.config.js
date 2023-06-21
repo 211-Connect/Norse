ddfef1a237b081b6e2202b09eead9c25abfaea7a
Chris Short <13677134+devcshort@users.noreply.github.com>
2023-06-20 17:09:10 -0700

    Update handling of documentation build

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/Norse/',
 
-  trailingSlash: true,
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
