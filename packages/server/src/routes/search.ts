125a497d70d3fcefedd3fc7129be10decc7b2f8c
Jay Hannah <jay.hannah@iinteractive.com>
2023-09-06 11:09:12 -0500

    Sometimes req.tenant is undefined
    For example, under Docker, tenant is 0, which causes a runtime
    error on these lines of code before this patch.

--- packages/server/src/routes/search.ts
+++ packages/server/src/routes/search.ts
@@ -42,7 +42,7 @@ router.get('/', async (req, res) => {
       typeof q.coords === 'string' ? q.coords.split(',') : q.coords;
     coords = coords instanceof Array && coords.length === 2 ? coords : null;
 
-    if (req.tenant.facets && req.tenant.facets instanceof Array) {
+    if (req.tenant && req.tenant.facets && req.tenant.facets instanceof Array) {
       // Get facets for faceted search for specific tenant
       req.tenant.facets?.forEach((data) => {
         aggs[data.facet] = {
@@ -202,7 +202,7 @@ router.get('/', async (req, res) => {
     const data = await ElasticClient.search(queryBuilder);
 
     const facets: any = {};
-    if (req.tenant.facets && req.tenant.facets instanceof Array) {
+    if (req.tenant && req.tenant.facets && req.tenant.facets instanceof Array) {
       for (const item of req.tenant.facets) {
         facets[item.facet] = item.name;
       }
@@ -216,7 +216,7 @@ router.get('/', async (req, res) => {
       level: 'search',
       message: 'search query',
       sessionId: req.headers['x-session-id'],
-      tenantId: req.tenant.tenantId,
+      tenantId: req.tenant ? req.tenant.tenantId : 0,
       search: {
         type: q.query_type,
         query: q.query_type === 'text' ? q.query : null,
