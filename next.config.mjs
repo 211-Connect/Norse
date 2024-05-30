87bc8cc3f52163a8a51778edee42588fc22945be
devcshort <13677134+devcshort@users.noreply.github.com>
2024-05-29 19:25:29 -0700

    Add public runtime environment variables

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,12 @@ const {
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n: rest,
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_MAPBOX_API_KEY: process.env.NEXT_PUBLIC_MAPBOX_API_KEY,
+    NEXT_PUBLIC_GTM_CONTAINER_ID: process.env.NEXT_PUBLIC_GTM_CONTAINER_ID,
+    NEXT_PUBLIC_KEYCLOAK_REALM: process.env.NEXT_PUBLIC_KEYCLOAK_REALM,
+    NEXT_PUBLIC_HELLO_WORLD: process.env.NEXT_PUBLIC_HELLO_WORLD,
+  },
 };
 
 const withBundleAnalyzer = bundleAnalyzer({
