6d2d41c3fa165ef91e1d1f2ac0b24cd37436a4f4
devcshort <13677134+devcshort@users.noreply.github.com>
2024-05-30 14:09:17 -0700

    Fallback to using app.defaults.json when app.config.json doesn't exist

--- src/lib/server/utils.ts
+++ src/lib/server/utils.ts
@@ -2,12 +2,27 @@ import fs from 'fs/promises';
 import path from 'path';
 
 export async function serverSideAppConfig() {
-  const rawData = await fs.readFile(path.resolve('./app.config.json'));
   let appConfig;
+  let rawData;
+  try {
+    await fs.stat(path.resolve('./app.config.json'));
+    rawData = await fs.readFile(path.resolve('./app.config.json'));
+  } catch (_err) {
+    // file doesn't exist. Don't do anything
+  }
+
+  if (!rawData) {
+    try {
+      rawData = await fs.readFile(path.resolve('./app.defaults.json'));
+    } catch (err) {
+      console.error('failed to read app.defaults.json', err);
+    }
+  }
+
   try {
     appConfig = JSON.parse(rawData.toString());
   } catch (err) {
-    console.error(err);
+    console.error('Unable to parse app.config', err);
     appConfig = {};
   }
 
