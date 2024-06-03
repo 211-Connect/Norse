ebdbe8a08b401f66bb89d14e45d67e776d59bd84
devcshort <13677134+devcshort@users.noreply.github.com>
2024-06-03 16:01:08 -0700

    Add fallback/default node url

--- src/lib/elasticsearch/index.ts
+++ src/lib/elasticsearch/index.ts
@@ -1,7 +1,7 @@
 import { Client } from '@elastic/elasticsearch';
 
 const elasticsearch = new Client({
-  node: process.env.ELASTICSEARCH_NODE as string,
+  node: (process.env?.ELASTICSEARCH_NODE as string) ?? 'http://127.0.0.1',
   auth: {
     apiKey: process.env.ELASTICSEARCH_API_KEY as string,
   },
