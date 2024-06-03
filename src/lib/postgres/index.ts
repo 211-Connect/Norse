8eb7e85e5edf1ddf8505b8d51c25564b241fe479
devcshort <13677134+devcshort@users.noreply.github.com>
2024-06-02 20:45:35 -0700

    Change postgres to have a custom output

--- src/lib/postgres/index.ts
+++ src/lib/postgres/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma-postgres';
 
 // PrismaClient is attached to the `global` object in development to prevent
 // exhausting your database connection limit.
