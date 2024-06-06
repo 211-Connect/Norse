5ca91f4f83912b911eada8197395a121aa4b4a55
devcshort <13677134+devcshort@users.noreply.github.com>
2024-06-06 14:45:41 -0700

    Taxonomy endpoint isnt' supposed to be protected by session

--- src/pages/api/taxonomy/index.ts
+++ src/pages/api/taxonomy/index.ts
@@ -1,17 +1,9 @@
 import { NextApiHandler } from 'next';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '../auth/[...nextauth]';
 import { getSearchAdapter } from '@/lib/adapters/search/get-search-adapter';
 import { SuggestConfig } from '@/lib/adapters/search/BaseSearchAdapter';
 
 const FavoriteListHandler: NextApiHandler = async (req, res) => {
-  const session = await getServerSession(req, res, authOptions);
   const searchAdapter = await getSearchAdapter();
-
-  if (!session) {
-    res.status(401);
-    return;
-  }
 
   if (req.method === 'GET') {
     try {
