9bfd53523b815b07fb64192f2c45a82035c50ae1
devcshort <13677134+devcshort@users.noreply.github.com>
2024-06-06 14:09:59 -0700

    Fix alert so its hidden when no values are added

--- src/components/home/alert.tsx
+++ src/components/home/alert.tsx
@@ -5,7 +5,8 @@ import { buttonVariants } from '../ui/button';
 export default function Alert() {
   const appConfig = useAppConfig();
 
-  if (appConfig.alert == null) return null;
+  if (appConfig.alert == null || Object.keys(appConfig.alert).length === 0)
+    return null;
 
   return (
     <div className="flex flex-col items-center justify-center gap-4 bg-red-600 p-4 text-center text-white sm:flex-row sm:text-left">
