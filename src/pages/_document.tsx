a090803fbeadd9ca4d8cd8fc539b800b3ef419c0
devcshort <13677134+devcshort@users.noreply.github.com>
2024-08-07 17:26:15 -0700

    Remove mantine from document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,20 +1,13 @@
-import { createGetInitialProps } from '@mantine/next';
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document';
 
-const getInitialProps = createGetInitialProps();
-
-export default class _Document extends Document {
-  static getInitialProps = getInitialProps;
-
-  render() {
-    return (
-      <Html>
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
