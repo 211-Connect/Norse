8eea508657c14203a0f6971d2cbe009532918449
devcshort <13677134+devcshort@users.noreply.github.com>
2024-08-29 12:56:43 -0700

    fix: taxonomy suggestions no longer get lost on search resubmit

--- src/shared/components/search/search-button.tsx
+++ src/shared/components/search/search-button.tsx
@@ -4,14 +4,49 @@ import { Button } from '../ui/button';
 import { SearchService } from '../../services/search-service';
 import { useAtomValue } from 'jotai';
 import { searchAtom } from '../../store/search';
+import { useDebounce } from '@/shared/hooks/use-debounce';
+import { useTaxonomies } from '@/shared/hooks/api/use-taxonomies';
+import { useSuggestions } from '@/shared/hooks/use-suggestions';
 
 export function SearchButton() {
   const { t } = useTranslation('common');
   const router = useRouter();
   const search = useAtomValue(searchAtom);
+  const debouncedSearchTerm = useDebounce(search.searchTerm, 200);
+  const { data: taxonomies } = useTaxonomies(debouncedSearchTerm);
+  const suggestions = useSuggestions();
+
+  // Find the taxonomy code to be used for a query
+  // Fallback to the original string value if a code isn't found
+  const findCode = (value: string) => {
+    const taxonomy = taxonomies.find(
+      (tax) => tax.name.toLowerCase() === value.toLowerCase(),
+    );
+    if (taxonomy) return taxonomy.code;
+
+    const suggestion = suggestions.find(
+      (sugg) => sugg.name.toLowerCase() === value.toLowerCase(),
+    );
+    if (suggestion) return suggestion.taxonomies;
+
+    return value;
+  };
+
+  const getQueryType = (value, query) => {
+    if (query.trim().length === 0) return '';
+    if (query === value) return 'text';
+    return 'taxonomy';
+  };
 
   const onClick = async () => {
-    const urlParams = SearchService.createUrlParamsForSearch(search);
+    const query = findCode(search.searchTerm);
+    const queryType = getQueryType(search.searchTerm, query);
+
+    const urlParams = SearchService.createUrlParamsForSearch({
+      ...search,
+      query,
+      queryType,
+    });
 
     await router.push({
       pathname: '/search',
