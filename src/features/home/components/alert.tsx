16e0de85442ae90ea79c7fffb5a1b2de0d7d7865
devcshort <13677134+devcshort@users.noreply.github.com>
2025-04-10 14:39:56 -0700

    fix: update for app dir

--- src/features/home/components/alert.tsx
+++ src/features/home/components/alert.tsx
@@ -5,12 +5,12 @@ import { cn } from '@/shared/lib/utils';
 import { buttonVariants } from '@/shared/components/ui/button';
 import { Alert as AlertComponent } from '@/shared/components/ui/alert';
 import { AlertCircle } from 'lucide-react';
+import { useConfigStore } from '@/lib/context/config-context/config-store-provider';
 
 export default function Alert() {
-  const appConfig = useAppConfig();
+  const alert = useConfigStore((config) => config.alert);
 
-  if (appConfig.alert == null || Object.keys(appConfig.alert).length === 0)
-    return null;
+  if (alert == null || Object.keys(alert).length === 0) return null;
 
   return (
     <div className="flex items-center justify-center p-8">
@@ -18,17 +18,16 @@ export default function Alert() {
         <div className="flex flex-col items-center justify-center gap-4 sm:flex-row">
           <AlertCircle className="size-8" />
 
-          <p className="text-lg font-semibold">{appConfig.alert.text}</p>
+          <p className="text-lg font-semibold">{alert.text}</p>
 
-          {appConfig.alert?.buttonText != null &&
-            appConfig.alert?.url != null && (
-              <Link
-                className={cn(buttonVariants({ variant: 'destructive' }))}
-                href={appConfig.alert.url}
-              >
-                {appConfig.alert.buttonText}
-              </Link>
-            )}
+          {alert?.buttonText != null && alert?.href != null && (
+            <Link
+              className={cn(buttonVariants({ variant: 'destructive' }))}
+              href={alert.href}
+            >
+              {alert.buttonText}
+            </Link>
+          )}
         </div>
       </AlertComponent>
     </div>
