331c9e5f163ae6a99878ea9c4d42551d710e7cc8
devcshort <13677134+devcshort@users.noreply.github.com>
2025-04-10 14:38:53 -0700

    feat: remove payload

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
-import { withPayload } from '@payloadcms/next/withPayload';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig = {};
 
 const withNextIntl = createNextIntlPlugin();
-export default withPayload(withNextIntl(nextConfig));
+export default withNextIntl(nextConfig);
