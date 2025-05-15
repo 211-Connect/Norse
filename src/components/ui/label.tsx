6b5a1cfb8d6fcfdcf78c86633aca93e4fd77162a
devcshort <13677134+devcshort@users.noreply.github.com>
2025-05-14 20:53:27 -0700

    fix: update components

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import * as LabelPrimitive from "@radix-ui/react-label"
 import { cva, type VariantProps } from "class-variance-authority"
 
-import { cn } from "@/shared/lib/utils"
+import { cn } from "@/lib/cn-utils"
 
 const labelVariants = cva(
   "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
