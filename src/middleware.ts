9e0c81d1f27fed3a6b6fb7e6a6f287f3838249c5
Maciej Łopalewski <112621777+u11d-maciej-lopalewski@users.noreply.github.com>
2025-07-03 13:14:15 +0200

    Fix/sessioncookies (#226)
    * fix: session cookies
    
    * fix: exclude adresources/api/auth from middlewere matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,7 +11,7 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    '/((?!api/auth|api|_next/static|_next/image|favicon.ico).*)',
+    '/((?!adresources/api/auth|api/auth|api|_next/static|_next/image|favicon.ico).*)',
   ],
 };
 
