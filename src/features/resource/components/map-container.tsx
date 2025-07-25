d1a63a50644fe84f345bfc6e47b210379eb16558
Maciej Łopalewski <maciej.lopalewski@uninterrupted.tech>
2025-07-25 12:05:51 +0200

    feat: disable map view if coordinates are default 0,0

--- src/features/resource/components/map-container.tsx
+++ src/features/resource/components/map-container.tsx
@@ -1,13 +1,26 @@
 import { MapRenderer } from '@/shared/components/map/map-renderer';
 
 export function MapContainer({ resource }) {
+  const coordinates = resource?.location?.coordinates;
+
+  // Check if coordinates are [0, 0]
+  const isZeroCoordinates =
+    Array.isArray(coordinates) &&
+    coordinates.length === 2 &&
+    coordinates[0] === 0 &&
+    coordinates[1] === 0;
+
+  if (!coordinates || isZeroCoordinates) {
+    return null;
+  }
+
   return (
     <div className="h-[500px] max-h-64 w-full print:hidden">
       <MapRenderer
         markers={[
           {
             id: resource.id,
-            coordinates: resource?.location?.coordinates,
+            coordinates,
           },
         ]}
         disableUserLocation
