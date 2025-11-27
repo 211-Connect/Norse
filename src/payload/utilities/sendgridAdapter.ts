0b066d820e8fb1cd0e2461bb26da950ef8d100ea
Paweł Sobolewski <pawel.sobolewski@uninterrupted.tech>
2025-11-28 00:44:45 +0100

    chore: fix sendgrid adapter

--- src/payload/utilities/sendgridAdapter.ts
+++ src/payload/utilities/sendgridAdapter.ts
@@ -5,7 +5,6 @@ interface SendGridTransportOptions {
 }
 
 export function sendGridTransport(options: SendGridTransportOptions) {
-  // Ustawienie API Key SendGrid
   sgMail.setApiKey(options.apiKey);
 
   return {
@@ -13,26 +12,121 @@ export function sendGridTransport(options: SendGridTransportOptions) {
     version: '1.0.0',
 
     async send(mail: any, callback: (err: Error | null, info?: any) => void) {
-      try {
-        const { from, to, subject, html, text } = mail.message;
+      mail.normalize((err, source) => {
+        if (err) {
+          return callback(err);
+        }
 
-        const msg = {
-          to,
-          from,
-          subject,
-          text: text || '',
-          html: html || '',
-        };
+        let msg: any = {};
+        Object.keys(source || {}).forEach((key) => {
+          switch (key) {
+            case 'subject':
+            case 'text':
+            case 'html':
+              msg[key] = source[key];
+              break;
+            case 'from':
+            case 'replyTo':
+              msg[key] = []
+                .concat(source[key] || [])
+                .map((entry: any) => ({
+                  name: entry.name,
+                  email: entry.address,
+                }))
+                .shift();
+              break;
+            case 'to':
+            case 'cc':
+            case 'bcc':
+              msg[key] = [].concat(source[key] || []).map((entry: any) => ({
+                name: entry.name,
+                email: entry.address,
+              }));
+              break;
+            case 'attachments':
+              {
+                let attachments = source.attachments.map((entry: any) => {
+                  let attachment: any = {
+                    content: entry.content,
+                    filename: entry.filename,
+                    type: entry.contentType,
+                    disposition: 'attachment',
+                  };
+                  if (entry.cid) {
+                    attachment.content_id = entry.cid;
+                    attachment.disposition = 'inline';
+                  }
+                  return attachment;
+                });
 
-        const response = await sgMail.send(msg);
+                msg.attachments = []
+                  .concat(msg.attachments || [])
+                  .concat(attachments);
+              }
+              break;
+            case 'alternatives':
+              {
+                let alternatives = source.alternatives.map((entry) => {
+                  let alternative = {
+                    content: entry.content,
+                    type: entry.contentType,
+                  };
+                  return alternative;
+                });
 
-        callback(null, {
-          envelope: { from, to },
-          messageId: response[0]?.headers['x-message-id'] || 'unknown',
+                msg.content = [].concat(msg.content || []).concat(alternatives);
+              }
+              break;
+            case 'icalEvent':
+              {
+                let attachment: any = {
+                  content: source.icalEvent.content,
+                  filename: source.icalEvent.filename || 'invite.ics',
+                  type: 'application/ics',
+                  disposition: 'attachment',
+                };
+                msg.attachments = []
+                  .concat(msg.attachments || [])
+                  .concat(attachment);
+              }
+              break;
+            case 'watchHtml':
+              {
+                let alternative: any = {
+                  content: source.watchHtml,
+                  type: 'text/watch-html',
+                };
+                msg.content = [].concat(msg.content || []).concat(alternative);
+              }
+              break;
+            case 'normalizedHeaders':
+              msg.headers = msg.headers || {};
+              Object.keys(source.normalizedHeaders || {}).forEach((header) => {
+                msg.headers[header] = source.normalizedHeaders[header];
+              });
+              break;
+            case 'messageId':
+              msg.headers = msg.headers || {};
+              msg.headers['message-id'] = source.messageId;
+              break;
+            default:
+              msg[key] = source[key];
+          }
         });
-      } catch (error) {
-        callback(error);
-      }
+
+        if (msg.content && msg.content.length) {
+          if (msg.text) {
+            msg.content.unshift({ type: 'text/plain', content: msg.text });
+            delete msg.text;
+          }
+          if (msg.html) {
+            msg.content.unshift({ type: 'text/html', content: msg.html });
+            delete msg.html;
+          }
+        }
+
+        sgMail.send(msg, undefined, callback);
+      });
     },
   };
 }
