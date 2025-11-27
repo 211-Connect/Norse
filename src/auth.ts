732ff09cb7a8524734fa5ec8ead3d143dead40d2
Paweł Sobolewski <pawel.sobolewski@uninterrupted.tech>
2025-11-27 21:27:37 +0100

    chore: remove logs

--- src/auth.ts
+++ src/auth.ts
@@ -8,7 +8,7 @@ import { Tenant } from './payload/payload-types';
 
 const isDebug = process.env.NEXTAUTH_DEBUG === 'true';
 
-export interface createAuthOptionsProps {
+export interface CreateAuthOptionsProps {
   baseUrl?: string;
   keycloak?: {
     issuer?: string;
@@ -21,151 +21,142 @@ const createAuthOptions = ({
   baseUrl,
   keycloak,
   secret,
-}: createAuthOptionsProps): NextAuthOptions => {
-  console.log('[NextAuth] createAuthOptions called with baseUrl:', baseUrl);
-  console.log(
-    '[NextAuth] CUSTOM BASE PATH:',
-    process.env.NEXT_PUBLIC_CUSTOM_BASE_PATH,
-  );
-  return {
-    callbacks: {
-      async redirect({ url, baseUrl: _baseUrl }) {
-        if (!baseUrl) return _baseUrl;
+}: CreateAuthOptionsProps): NextAuthOptions => ({
+  callbacks: {
+    async redirect({ url, baseUrl: _baseUrl }) {
+      if (!baseUrl) return _baseUrl;
 
-        if (url.startsWith('/')) return `${baseUrl}${url}`;
-        if (new URL(url).origin === baseUrl) return url;
-        return baseUrl;
-      },
-      session({ session, token }) {
-        session.user.accessToken = token.accessToken;
-        session.user.refreshToken = token.refreshToken;
-        session.user = omitBy(session.user, isNil) as Session['user'];
-        if (token.error) {
-          session.error = token.error;
-        }
-        return session;
-      },
-      async jwt({ token, account }) {
-        token = omitBy(token, isNil);
+      if (url.startsWith('/')) return `${baseUrl}${url}`;
+      if (new URL(url).origin === baseUrl) return url;
+      return baseUrl;
+    },
+    session({ session, token }) {
+      session.user.accessToken = token.accessToken;
+      session.user.refreshToken = token.refreshToken;
+      session.user = omitBy(session.user, isNil) as Session['user'];
+      if (token.error) {
+        session.error = token.error;
+      }
+      return session;
+    },
+    async jwt({ token, account }) {
+      token = omitBy(token, isNil);
 
-        if (account) {
-          // Initial sign in
+      if (account) {
+        // Initial sign in
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          expiresAt: account.expires_at,
+        };
+      } else if (Date.now() < (token?.expiresAt ?? 0) * 1000) {
+        // Token is still valid
+        return token;
+      } else {
+        // Token expired. Refresh it.
+        try {
+          const response = await axios.post(
+            `${keycloak?.issuer}/protocol/openid-connect/token`,
+            {
+              client_id: process.env.KEYCLOAK_CLIENT_ID,
+              client_secret: keycloak?.clientSecret,
+              grant_type: 'refresh_token',
+              refresh_token: token.refreshToken,
+            },
+            {
+              headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+              },
+            },
+          );
+
           return {
             ...token,
-            accessToken: account.access_token,
-            refreshToken: account.refresh_token,
-            expiresAt: account.expires_at,
+            accessToken: response.data.access_token,
+            refreshToken: response.data.refresh_token,
+            expiresAt: Math.floor(Date.now() / 1000 + response.data.expires_in),
           };
-        } else if (Date.now() < (token?.expiresAt ?? 0) * 1000) {
-          // Token is still valid
-          return token;
-        } else {
-          // Token expired. Refresh it.
-          try {
-            const response = await axios.post(
-              `${keycloak?.issuer}/protocol/openid-connect/token`,
-              {
-                client_id: process.env.KEYCLOAK_CLIENT_ID,
-                client_secret: keycloak?.clientSecret,
-                grant_type: 'refresh_token',
-                refresh_token: token.refreshToken,
-              },
-              {
-                headers: {
-                  'Content-Type': 'application/x-www-form-urlencoded',
-                },
-              },
+        } catch (err: any) {
+          if (axios.isAxiosError(err)) {
+            console.error(
+              'Error refreshing access token :',
+              err.response?.data,
             );
-
-            return {
-              ...token,
-              accessToken: response.data.access_token,
-              refreshToken: response.data.refresh_token,
-              expiresAt: Math.floor(
-                Date.now() / 1000 + response.data.expires_in,
-              ),
-            };
-          } catch (err: any) {
-            if (axios.isAxiosError(err)) {
-              console.error(
-                'Error refreshing access token :',
-                err.response?.data,
-              );
-            }
-
-            token.error = 'RefreshAccessTokenError';
-            return token;
           }
+
+          token.error = 'RefreshAccessTokenError';
+          return token;
         }
+      }
+    },
+  },
+  events: {
+    signOut: async (message) => {
+      await axios.post(
+        `${keycloak?.issuer}/protocol/openid-connect/logout`,
+        {
+          client_id: process.env.KEYCLOAK_CLIENT_ID,
+          client_secret: keycloak?.clientSecret,
+          refresh_token: message.token.refreshToken,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        },
+      );
+    },
+  },
+  providers: [
+    KeycloakProvider({
+      authorization: {
+        params: {
+          redirect_uri: `${baseUrl}/api/auth/callback/keycloak`,
+        },
       },
-    },
-    events: {
-      signOut: async (message) => {
-        await axios.post(
-          `${keycloak?.issuer}/protocol/openid-connect/logout`,
-          {
-            client_id: process.env.KEYCLOAK_CLIENT_ID,
-            client_secret: keycloak?.clientSecret,
-            refresh_token: message.token.refreshToken,
-          },
-          {
-            headers: {
-              'Content-Type': 'application/x-www-form-urlencoded',
-            },
-          },
-        );
+      clientId: process.env?.KEYCLOAK_CLIENT_ID ?? '',
+      clientSecret: keycloak?.clientSecret ?? '',
+      issuer: keycloak?.issuer,
+    }),
+    // TODO :: Implement custom Keycloak integration so we can have integrated signup/signout pages
+    CredentialsProvider({
+      name: 'CustomKeycloak',
+      credentials: {
+        email: {
+          label: 'email',
+          type: 'email',
+          placeholder: 'hello world',
+        },
       },
-    },
-    providers: [
-      KeycloakProvider({
-        authorization: {
-          params: {
-            redirect_uri: `${baseUrl}/api/auth/callback/keycloak`,
-          },
-        },
-        clientId: process.env?.KEYCLOAK_CLIENT_ID ?? '',
-        clientSecret: keycloak?.clientSecret ?? '',
-        issuer: keycloak?.issuer,
-      }),
-      // TODO :: Implement custom Keycloak integration so we can have integrated signup/signout pages
-      CredentialsProvider({
-        name: 'CustomKeycloak',
-        credentials: {
-          email: {
-            label: 'email',
-            type: 'email',
-            placeholder: 'hello world',
-          },
-        },
-        async authorize(_credentials, _req) {
-          throw new Error('Not implemented yet.');
-        },
-      }),
-    ],
-    pages: {
-      error: `${process.env.NEXT_PUBLIC_CUSTOM_BASE_PATH || ''}/auth/error`,
-      newUser: `${process.env.NEXT_PUBLIC_CUSTOM_BASE_PATH || ''}/auth/new-user`,
-      signIn: `${process.env.NEXT_PUBLIC_CUSTOM_BASE_PATH || ''}/auth/sign-in`,
-      signOut: `${process.env.NEXT_PUBLIC_CUSTOM_BASE_PATH || ''}/auth/sign-out`,
-      verifyRequest: `${process.env.NEXT_PUBLIC_CUSTOM_BASE_PATH || ''}/auth/verify-request`,
-    },
-    secret,
-    ...(isDebug && {
-      debug: true,
-      logger: {
-        error(code, metadata) {
-          console.error('[NextAuth][error]', code, metadata);
-        },
-        warn(code) {
-          console.warn('[NextAuth][warn]', code);
-        },
-        debug(code, metadata) {
-          console.debug('[NextAuth][debug]', code, metadata);
-        },
+      async authorize(_credentials, _req) {
+        throw new Error('Not implemented yet.');
       },
     }),
-  };
-};
+  ],
+  pages: {
+    error: `${process.env.NEXT_PUBLIC_CUSTOM_BASE_PATH || ''}/auth/error`,
+    newUser: `${process.env.NEXT_PUBLIC_CUSTOM_BASE_PATH || ''}/auth/new-user`,
+    signIn: `${process.env.NEXT_PUBLIC_CUSTOM_BASE_PATH || ''}/auth/sign-in`,
+    signOut: `${process.env.NEXT_PUBLIC_CUSTOM_BASE_PATH || ''}/auth/sign-out`,
+    verifyRequest: `${process.env.NEXT_PUBLIC_CUSTOM_BASE_PATH || ''}/auth/verify-request`,
+  },
+  secret,
+  ...(isDebug && {
+    debug: true,
+    logger: {
+      error(code, metadata) {
+        console.error('[NextAuth][error]', code, metadata);
+      },
+      warn(code) {
+        console.warn('[NextAuth][warn]', code);
+      },
+      debug(code, metadata) {
+        console.debug('[NextAuth][debug]', code, metadata);
+      },
+    },
+  }),
+});
 
 const getSession = (
   protocol: string,
