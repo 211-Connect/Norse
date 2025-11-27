80ae0768b7e03ebe468b80c6cb416007419e8d64
Paweł Sobolewski <pawel.sobolewski@uninterrupted.tech>
2025-11-27 22:39:35 +0100

    chore: add logs

--- src/app/(app)/shared/serverActions/favorites/searchFavoriteLists.ts
+++ src/app/(app)/shared/serverActions/favorites/searchFavoriteLists.ts
@@ -1,30 +1,30 @@
 'use server';
 
-import { cache } from 'react';
 import { createAxiosWithAuth } from '../../lib/axiosWithAuth';
 import { API_URL, FAVORITES_LIST_ENDPOINT } from '../../lib/constants';
 
-export const searchFavoriteLists = cache(
-  async (searchText: string, tenantId?: string) => {
-    try {
-      console.log(searchText, tenantId);
-      console.log(createAxiosWithAuth({ tenantId }));
-      const { data } = await createAxiosWithAuth({ tenantId }).get(
-        `${API_URL}/${FAVORITES_LIST_ENDPOINT}/search`,
-        {
-          params: {
-            name: searchText,
-          },
-          headers: {
-            'x-api-version': '1',
-          },
+export const searchFavoriteLists = async (
+  searchText: string,
+  tenantId?: string,
+) => {
+  try {
+    console.log(searchText, tenantId);
+    console.log(createAxiosWithAuth({ tenantId }));
+    const { data } = await createAxiosWithAuth({ tenantId }).get(
+      `${API_URL}/${FAVORITES_LIST_ENDPOINT}/search`,
+      {
+        params: {
+          name: searchText,
         },
-      );
+        headers: {
+          'x-api-version': '1',
+        },
+      },
+    );
 
-      return data;
-    } catch (err) {
-      console.error('Error searching favorite lists:', err);
-      return [];
-    }
-  },
-);
+    return data;
+  } catch (err) {
+    console.error('Error searching favorite lists:', err);
+    return [];
+  }
+};
