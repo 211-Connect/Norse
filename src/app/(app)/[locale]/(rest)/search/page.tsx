33309f8b0cee45d10531e31fef367b0257d52848
David Botos <dbotos@umich.edu>
2026-01-26 18:28:33 -0800

    adding feature flagged use of the findResourcesV2 method

--- src/app/(app)/[locale]/(rest)/search/page.tsx
+++ src/app/(app)/[locale]/(rest)/search/page.tsx
@@ -5,7 +5,8 @@ import { ResultsSection } from '@/app/(app)/features/search/components/results-s
 import { PageWrapper } from '@/app/(app)/shared/components/page-wrapper';
 import initTranslations from '@/app/(app)/shared/i18n/i18n';
 import { getServerDevice } from '@/app/(app)/shared/lib/get-server-device';
-import { findResources } from '@/app/(app)/shared/services/search-service';
+import { findResources, findResourcesV2 } from '@/app/(app)/shared/services/search-service';
+import { geocodeLocationCached } from '@/app/(app)/shared/services/geocoding-service';
 import { getCookies } from 'cookies-next/server';
 import { cookies, headers } from 'next/headers';
 import { Metadata } from 'next/types';
