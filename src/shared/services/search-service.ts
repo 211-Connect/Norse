bab24622f462a877909c71a877bc3e221f3eaa28
Michał Miler <michal.miler@uninterrupted.tech>
2026-01-28 14:33:12 +0100

    feat: filters with localized data

--- src/shared/services/search-service.ts
+++ src/shared/services/search-service.ts
@@ -105,6 +105,19 @@ export class SearchService {
           : (data?.search?.hits?.total ?? 0);
     }
 
+    const aggregations = data?.search?.aggregations ?? {};
+    const filters = Object.keys(aggregations).reduce((acc, key) => {
+      const buckets = aggregations[key]?.buckets;
+      if (
+        !key.startsWith('label_') &&
+        Array.isArray(buckets) &&
+        buckets.length > 0
+      ) {
+        acc[key] = aggregations[key];
+      }
+      return acc;
+    }, {});
+
     return {
       results:
         data?.search?.hits?.hits?.map((hit: any) => {
@@ -149,7 +162,7 @@ export class SearchService {
       noResults,
       totalResults,
       page,
-      filters: data?.search?.aggregations ?? {},
+      filters,
     };
   }
 }
