2d9fe4cd240610fca70c00d8e7ff37a3599c7ed7
Michał Miler <michal.miler@uninterrupted.tech>
2026-02-06 08:14:29 +0100

    chore: add "sandbox" attribute to gtm iframe

--- src/app/(app)/shared/components/google-tag-manager-script.tsx
+++ src/app/(app)/shared/components/google-tag-manager-script.tsx
@@ -26,6 +26,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 height="0"
                 width="0"
                 style="display:none;visibility:hidden"
+                sandbox
               ></iframe>`,
         }}
       />
