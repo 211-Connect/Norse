c41eeecec97b0b0237f722e929917b6e0a2fe14b
Michał Miler <128463783+u11d-michal-miler@users.noreply.github.com>
2026-02-25 15:00:35 +0100

    chore: add types for topics (#398)

--- src/app/(app)/shared/hooks/use-topics.ts
+++ src/app/(app)/shared/hooks/use-topics.ts
@@ -2,20 +2,6 @@
 
 import { useAppConfig } from './use-app-config';
 
-export type SubTopic = {
-  name: string;
-  href?: string;
-  query?: string;
-  queryType?: string;
-};
-
-export type Topic = {
-  name: string;
-  href?: string;
-  image?: string;
-  subtopics?: SubTopic[];
-};
-
 export function useTopics() {
   const appConfig = useAppConfig();
   return appConfig.topics?.list;
