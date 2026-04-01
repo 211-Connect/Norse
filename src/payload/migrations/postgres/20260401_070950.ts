dc6a8f76e921c5cccbc0ea2ec8c1c11f7300c589
Michał Miler <michal.miler@uninterrupted.tech>
2026-04-01 09:39:21 +0200

    fix formatting

--- src/payload/migrations/postgres/20260401_070950.ts
+++ src/payload/migrations/postgres/20260401_070950.ts
@@ -3,7 +3,7 @@ import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres';
 export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
    CREATE TYPE "public"."enum_tenants_sms_sms_provider" AS ENUM('Twilio', 'EMS');
-  ALTER TABLE "tenants" ADD COLUMN "sms_sms_provider" "enum_tenants_sms_sms_provider" DEFAULT 'Twilio';
+  ALTER TABLE "tenants" ADD COLUMN "sms_sms_provider" "enum_tenants_sms_sms_provider";
   ALTER TABLE "tenants" ADD COLUMN "sms_twilio_phone_number" varchar;
   ALTER TABLE "tenants" ADD COLUMN "sms_twilio_api_key" varchar;
   ALTER TABLE "tenants" ADD COLUMN "sms_twilio_api_key_sid" varchar;
@@ -11,9 +11,9 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   ALTER TABLE "tenants" ADD COLUMN "sms_ems_api_key" varchar;
   ALTER TABLE "tenants" ADD COLUMN "sms_ems_short_code" varchar;
   ALTER TABLE "tenants" ADD COLUMN "sms_ems_keyword" varchar;
-  
+
   UPDATE "tenants"
-  SET 
+  SET
     "sms_twilio_phone_number" = "twilio_phone_number",
     "sms_twilio_api_key" = "twilio_api_key",
     "sms_twilio_api_key_sid" = "twilio_api_key_sid",
