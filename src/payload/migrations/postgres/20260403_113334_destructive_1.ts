cbc59924eeb86b1eb437a4ae00d774d265af7c45
Michał Miler <128463783+u11d-michal-miler@users.noreply.github.com>
2026-04-08 12:33:01 +0200

    fix alert migrations (#586)
    * fix alert migrations
    
    * update index

---
+++ src/payload/migrations/postgres/20260403_113334_destructive_1.ts
@@ -0,0 +1,21 @@
+import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres';
+
+export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
+  await db.execute(sql`
+  DROP INDEX "rds_common_alert_locale_idx";
+  DROP INDEX "rds_new_layout_callouts_options_locale_idx";
+  DROP INDEX "_rds_v_version_common_alert_locale_idx";
+  DROP INDEX "_rds_v_version_new_layout_callouts_options_locale_idx";`);
+}
+
+export async function down({
+  db,
+  payload,
+  req,
+}: MigrateDownArgs): Promise<void> {
+  await db.execute(sql`
+  CREATE INDEX "rds_common_alert_locale_idx" ON "rds_common_alert" USING btree ("_locale");
+  CREATE INDEX "rds_new_layout_callouts_options_locale_idx" ON "rds_new_layout_callouts_options" USING btree ("_locale");
+  CREATE INDEX "_rds_v_version_common_alert_locale_idx" ON "_rds_v_version_common_alert" USING btree ("_locale");
+  CREATE INDEX "_rds_v_version_new_layout_callouts_options_locale_idx" ON "_rds_v_version_new_layout_callouts_options" USING btree ("_locale");`);
+}
