d33dffb431d414d56c08753986b9d5d40ff80853
David Botos <dbotos@umich.edu>
2026-04-13 11:20:51 -0700

    (a11y) - keyboarding outline header fix

--- src/app/(app)/shared/components/header.tsx
+++ src/app/(app)/shared/components/header.tsx
@@ -335,9 +335,9 @@ export function Header() {
         <nav
           id={HEADER_DESKTOP_ID}
           aria-label="Primary"
-          className="z-[1] ml-auto hidden w-fit justify-end overflow-hidden xl:flex"
+          className="z-[1] ml-auto hidden w-fit justify-end xl:flex"
         >
-          <ul className="flex items-center gap-6 overflow-x-auto overflow-y-hidden">
+          <ul className="flex items-center gap-6 overflow-x-auto px-1 py-2 sm:px-2">
             {sitemap}
           </ul>
         </nav>
