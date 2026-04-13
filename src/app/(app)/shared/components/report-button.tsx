ed58665aff7e21de33ca53c10bb7564223064b42
Michał Miler <michal.miler@uninterrupted.tech>
2026-04-13 13:36:13 +0200

    revert report button changes

--- src/app/(app)/shared/components/report-button.tsx
+++ src/app/(app)/shared/components/report-button.tsx
@@ -3,32 +3,12 @@
 import { TriangleAlert } from 'lucide-react';
 import { useTranslation } from 'react-i18next';
 import { useEffect, useState } from 'react';
-import { usePathname } from 'next/navigation';
 
 import { useAppConfig } from '../hooks/use-app-config';
 import { buttonVariants } from './ui/button';
 import { cn } from '../lib/utils';
 import { Link } from './link';
 import { NEW_TAB_WARNING } from '../lib/constants';
-
-function buildFeedbackHref(
-  feedbackUrlValue: string | undefined,
-  referringUrl?: string,
-) {
-  if (!feedbackUrlValue) {
-    return null;
-  }
-
-  const currentUrl = new URL(feedbackUrlValue);
-  const baseUrl = currentUrl.toString().split('?')[0];
-  const urlParams = new URLSearchParams(currentUrl.searchParams);
-
-  if (referringUrl) {
-    urlParams.set('referring_url', referringUrl);
-  }
-
-  return `${baseUrl}?${urlParams.toString()}`;
-}
 
 export function ReportButton({
   className,
@@ -37,7 +17,6 @@ export function ReportButton({
   className?: string;
   customText?: string;
 }) {
-  const pathname = usePathname();
   const appConfig = useAppConfig();
   const { t } = useTranslation('page-resource');
   const feedbackUrlValue = appConfig?.contact?.feedbackUrl;
@@ -51,8 +30,14 @@ export function ReportButton({
       return;
     }
 
-    setHref(buildFeedbackHref(feedbackUrlValue, window.location.href));
-  }, [feedbackUrlValue, pathname]);
+    const currentUrl = new URL(feedbackUrlValue);
+    const baseUrl = currentUrl.toString().split('?')[0];
+    const urlParams = new URLSearchParams(currentUrl.searchParams);
+
+    urlParams.set('referring_url', window.location.href);
+
+    setHref(`${baseUrl}?${urlParams.toString()}`);
+  }, [feedbackUrlValue]);
 
   if (!feedbackUrlValue) {
     return null;
