9f26a35ed7986448b1054aed670c63e82d727087
Michał Miler <michal.miler@uninterrupted.tech>
2026-04-13 13:47:16 +0200

    simplify PromptAuthDialog

--- src/app/(app)/shared/components/global-dialogs/promt-auth-dialog.tsx
+++ src/app/(app)/shared/components/global-dialogs/promt-auth-dialog.tsx
@@ -1,17 +1,10 @@
 'use client';
 
 import { useAtomValue, useSetAtom } from 'jotai';
-import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { dialogsAtom, promptAuthAtom } from '@/app/(app)/shared/store/dialogs';
 
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-} from '../ui/dialog';
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from '../ui/dialog';
 import { Button } from '../ui/button';
 import { cn } from '../../lib/utils';
 import { buttonVariants } from '../ui/button';
@@ -19,14 +12,7 @@ import { buttonVariants } from '../ui/button';
 export function PromptAuthDialog() {
   const setState = useSetAtom(dialogsAtom);
   const state = useAtomValue(promptAuthAtom);
-  const [loginHref, setLoginHref] = useState('/api/auth/signin/keycloak');
-
   const { t } = useTranslation('common');
-
-  useEffect(() => {
-    const callbackUrl = encodeURIComponent(window.location.href);
-    setLoginHref(`/api/auth/signin/keycloak?callbackUrl=${callbackUrl}`);
-  }, []);
 
   const handleOpenChange = (value: boolean) => {
     setState((prev) => ({
@@ -38,28 +24,29 @@ export function PromptAuthDialog() {
     }));
   };
 
+  const loginHref = `/api/auth/signin/keycloak?callbackUrl=${encodeURIComponent(
+    typeof window !== 'undefined' ? window.location.href : '',
+  )}`;
+
   return (
     <Dialog open={state.open} onOpenChange={handleOpenChange}>
       <DialogContent>
         <DialogHeader>
           <DialogTitle>{t('modal.prompt_auth')}</DialogTitle>
-          <DialogDescription />
         </DialogHeader>
-        <div className="flex justify-end">
-          <div className="grid grid-cols-2 gap-2">
-            <Button onClick={() => handleOpenChange(false)} variant="outline">
-              {t('call_to_action.cancel')}
-            </Button>
+        <div className="flex justify-end gap-2">
+          <Button onClick={() => handleOpenChange(false)} variant="outline">
+            {t('call_to_action.cancel')}
+          </Button>
 
-            <a
-              className={cn(buttonVariants(), 'inline-flex')}
-              href={loginHref}
-              data-testid="login-btn"
-              onClick={() => handleOpenChange(false)}
-            >
-              {t('call_to_action.login')}
-            </a>
-          </div>
+          <a
+            className={cn(buttonVariants(), 'inline-flex')}
+            href={loginHref}
+            data-testid="login-btn"
+            onClick={() => handleOpenChange(false)}
+          >
+            {t('call_to_action.login')}
+          </a>
         </div>
       </DialogContent>
     </Dialog>
