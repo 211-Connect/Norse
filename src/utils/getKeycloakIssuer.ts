b6c38058bef60fc84f541dbc6d7e6ce696e7e28c
Bartek Gałęzowski <bartlomiej.galezowski@u11d.com>
2026-05-04 12:16:34 +0200

    feat: set Keycloak issuer base URL in environment variables

--- src/utils/getKeycloakIssuer.ts
+++ src/utils/getKeycloakIssuer.ts
@@ -1,2 +1,2 @@
 export const getKeycloakIssuer = (realm: string) =>
-  `https://auth.c211.io/realms/${realm}`;
+  `${process.env.KEYCLOAK_ISSUER_BASE_URL ?? 'https://auth.c211.io'}/realms/${realm}`;
