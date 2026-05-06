f81e11809b565fb962d570511a7b768dc5b94304
Michał Miler <michal.miler@uninterrupted.tech>
2026-05-06 07:14:10 +0200

    feat: push branding to keycloak

--- src/app/(app)/shared/components/header.tsx
+++ src/app/(app)/shared/components/header.tsx
@@ -225,6 +225,7 @@ export function Header() {
               )}
               variant="outline"
               onClick={() => {
+                start();
                 signOut({ redirect: true, callbackUrl: '/' });
               }}
             >
