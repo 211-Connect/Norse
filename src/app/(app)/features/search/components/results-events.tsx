3452b42eadff05519ff8dd1dff5b520b728c4e94
Bartek Gałęzowski <139535036+u11d-bartlomiej-galezowski@users.noreply.github.com>
2026-05-07 15:30:26 +0200

    hotfix: integrate user and search coordinates tracking in search dialog (#687)

--- src/app/(app)/features/search/components/results-events.tsx
+++ src/app/(app)/features/search/components/results-events.tsx
@@ -4,12 +4,7 @@ import { useAppConfig } from '@/app/(app)/shared/hooks/use-app-config';
 import { useClientSearchParams } from '@/app/(app)/shared/hooks/use-client-search-params';
 import { createResultsEvent } from '@/app/(app)/shared/lib/google-tag-manager';
 import { trackUmamiEvent, UmamiEvent } from '@/app/(app)/shared/lib/umami';
-import { useAtomValue } from 'jotai';
 import { useEffect } from 'react';
-import {
-  searchCoordinatesAtom,
-  userCoordinatesAtom,
-} from '../../../shared/store/search';
 
 interface ResultsEventsProps {
   results: any;
@@ -23,33 +18,12 @@ export const ResultsEvents = ({
   const { searchParamsObject } = useClientSearchParams();
   const appConfig = useAppConfig();
 
-  const userCoordinates = useAtomValue(userCoordinatesAtom);
-  const searchCoordinates = useAtomValue(searchCoordinatesAtom);
-
   useEffect(() => {
     createResultsEvent(
       { results, total: totalResults },
       searchParamsObject,
       appConfig.sessionId,
     );
-
-    const payload = {
-      userCoordinates: userCoordinates.join(',') ?? '',
-      searchCoordinates: searchCoordinates.join(',') ?? '',
-      query: String(searchParamsObject.query ?? ''),
-      queryLabel: String(searchParamsObject.query_label ?? ''),
-      tenantId: appConfig.tenantId ?? '',
-    };
-
-    if (searchParamsObject.query_type === 'taxonomy') {
-      console.log('Tracking taxonomy search click with payload:', payload);
-      trackUmamiEvent(UmamiEvent.SearchTaxonomy, payload);
-    }
-
-    if (searchParamsObject.query_type === 'text') {
-      console.log('Tracking text search click with payload:', payload);
-      trackUmamiEvent(UmamiEvent.SearchText, payload);
-    }
 
     if (totalResults === 0) {
       trackUmamiEvent(UmamiEvent.SearchZeroResults, {
@@ -61,10 +35,8 @@ export const ResultsEvents = ({
     appConfig.sessionId,
     appConfig.tenantId,
     results,
-    searchCoordinates,
     searchParamsObject,
     totalResults,
-    userCoordinates,
   ]);
 
   return null;
