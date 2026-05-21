030a38f4336db802c97b982218cedc952cff6bb9
Michał Miler <michal.miler@uninterrupted.tech>
2026-05-21 10:16:39 +0200

    feat(iss-904): feature flag to enable anonymous collections and printed collection variants)

--- src/app/(app)/shared/utils/appConfig.ts
+++ src/app/(app)/shared/utils/appConfig.ts
@@ -228,7 +228,7 @@ async function getAppConfigBase(
       contact: {},
       sms: null,
       featureFlags: {
-        anonymousCollectionsEnabled: true,
+        anonymousCollectionsEnabled: false,
         requireUserLocation: false,
         showFeedbackButtonGlobal: false,
         showFeedbackButtonOnResourcePages: false,
@@ -377,7 +377,8 @@ async function getAppConfigBase(
     },
     sms: getSmsConfig(resourceDirectory),
     featureFlags: {
-      anonymousCollectionsEnabled: true,
+      anonymousCollectionsEnabled:
+        resourceDirectory.featureFlags?.anonymousCollectionsEnabled ?? false,
       requireUserLocation:
         resourceDirectory.featureFlags?.requireUserLocation ?? false,
       showFeedbackButtonGlobal:
