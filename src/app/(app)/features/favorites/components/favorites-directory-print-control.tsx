07280abcffee52522d06517414305debbc2583e1
Michał Miler <michal.miler@uninterrupted.tech>
2026-05-27 13:58:39 +0200

    printable collection improvements

--- src/app/(app)/features/favorites/components/favorites-directory-print-control.tsx
+++ src/app/(app)/features/favorites/components/favorites-directory-print-control.tsx
@@ -43,7 +43,7 @@ export function FavoritesDirectoryPrintControl({
         open={open}
         onOpenChange={setOpen}
         data={data}
-        restoreFocusElement={triggerRef.current}
+        onRestoreFocus={() => triggerRef.current?.focus()}
       />
     </>
   );
