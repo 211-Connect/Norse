deed6fb9b3a358be80dda58bec849b694eb0036c
Michał Miler <128463783+u11d-michal-miler@users.noreply.github.com>
2026-07-22 12:12:19 +0200

    fix: fetching resource (#897)

--- src/app/(app)/shared/services/resource-service.ts
+++ src/app/(app)/shared/services/resource-service.ts
@@ -232,7 +232,7 @@ export async function getResource(
   locale: string,
   tenantId?: string,
 ): Promise<Resource | null> {
-  const url = `${API_URL}/resource/${id}?locale=${encodeURIComponent(locale)}&tenant_id=${encodeURIComponent(tenantId ?? '')}`;
+  const url = `${API_URL}/resource/${id}`;
   return fetchAndTransformResource(url, {
     locale,
     tenantId,
@@ -245,7 +245,7 @@ export async function getResourceByOriginalId(
   locale: string,
   tenantId?: string,
 ): Promise<Resource | null> {
-  const url = `${API_URL}/resource/original/${originalId}?locale=${encodeURIComponent(locale)}&tenant_id=${encodeURIComponent(tenantId ?? '')}`;
+  const url = `${API_URL}/resource/original/${originalId}`;
   return fetchAndTransformResource(url, {
     locale,
     tenantId,
