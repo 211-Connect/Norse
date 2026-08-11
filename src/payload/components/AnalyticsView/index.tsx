b5731eeb62f44351816dece48c00015e5bbeca75
Bartek Gałęzowski <bartlomiej.galezowski@u11d.com>
2026-08-11 09:10:24 +0200

    feat: enable ExportCSVButton in AnalyticsView and update CSV export headers

--- src/payload/components/AnalyticsView/index.tsx
+++ src/payload/components/AnalyticsView/index.tsx
@@ -6,7 +6,7 @@ import { Fragment } from 'react';
 import DateRange from '../analytics/DateRange';
 import TenantAutoSelect from '../analytics/TenantAutoSelect';
 import { ANALYTICS_DEFAULT_LAYOUT } from '../analytics/widgetInfo';
-// import ExportCSVButton from '../analytics/ExportCSVButton';
+import ExportCSVButton from '../analytics/ExportCSVButton';
 
 export default function AnalyticsView(props: AdminViewServerProps) {
   const user = props.user ?? (props.initPageResult?.req?.user as any);
@@ -66,7 +66,7 @@ export default function AnalyticsView(props: AdminViewServerProps) {
             }}
           >
             {/* The ExportCSVButton is temporarily disabled until we will stabilize a solution for exporting analytics data. */}
-            {/* <ExportCSVButton /> */}
+            <ExportCSVButton />
             <DateRange />
           </div>
         </div>
